084f75c5a843b1bc1ef4b5b2691cfa91f50e7571
Tikhonov Vladimir <goldenfox511@gmail.com>
2022-09-19 23:54:58 +0300

    Mongo init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [],
+    imports: [
+        MongooseModule.forRoot(
+            'mongodb://developer:Tester01@rc1b-6fuift4umjrvb4fj.mdb.yandexcloud.net:27018/?replicaSet=rs01&authSource=bump-development',
+        ),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
