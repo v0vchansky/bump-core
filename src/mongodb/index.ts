4ed5ae6e6c74b5601c682788e5d3193ea74b351b
Tikhonov Vladimir <goldenfox511@gmail.com>
2022-09-21 21:26:18 +0300

    Рефакторинг env

--- src/mongodb/index.ts
+++ src/mongodb/index.ts
@@ -2,11 +2,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 export const mongodb = MongooseModule.forRootAsync({
     useFactory: () => {
-        const CERT_PATH = process.env.MONGO_DB_CERT_PATH;
-        const USER_NAME = process.env.MONGO_USER_NAME;
-        const USER_PASS = process.env.MONGO_USER_PASS;
-        const HOST = process.env.MONGO_HOST;
-        const DB_NAME = process.env.MONGO_DB_NAME;
+        const {
+            MONGO_DB_CERT_PATH: CERT_PATH,
+            MONGO_USER_NAME: USER_NAME,
+            MONGO_USER_PASS: USER_PASS,
+            MONGO_HOST: HOST,
+            MONGO_DB_NAME: DB_NAME,
+        } = process.env;
 
         const uri = `mongodb://${USER_NAME}:${USER_PASS}@${HOST}/${DB_NAME}?replicaSet=rs01&authSource=${DB_NAME}&ssl=true`;
 
